53643b5f3556066f1273ca895b26bd08bce75ee0
jawaadmerali <113218507+jawaadmerali@users.noreply.github.com>
2023-08-11 19:01:43 -0500

    Create mvt-article.presentational.vue

---
+++ mvt-article.presentational.vue
@@ -0,0 +1,26 @@
+<template>
+    <a class="mvt-article-presentational panel-card vertical">
+        <div class="profile">
+            <img class="bg lazyload img-load-success" :src="image" :alt="imageAltText" />
+        </div>
+        <div class="singleline">
+            <b class="f4">
+                {{ headerText }}
+            </b>
+        </div>
+        <div class="multipleline-3 f7" v-html="descriptionText"></div>
+        <div class="singleline f7">{{ footerText }}</div>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'ArticlePresentational',
+    props: {
+        image: String,
+        headerText: String,
+        descriptionText: String,
+        footerText: String,
+    },
+};
+</script>
